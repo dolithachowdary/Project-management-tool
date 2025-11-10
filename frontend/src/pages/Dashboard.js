ed821e749c0c265e4537adbcb0c21576acc2d4e4
dolithachowdary <dolithadasari1209@gmail.com>
2025-11-10 16:11:14 +0530

    login , roles , dev dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
-import DashboardContent from "../components/DashboardContent";
- 
-function Dashboard({ role = "Project Manager" }) {
+import DashboardPM from "../components/DashboardPM";
+import DashboardDev from "../components/DashboardDev";
+
+
+function Dashboard() {
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData?.role) {
+      setRole(userData.role);
+    } else {
+      // If no user data found, redirect back to login
+      window.location.href = "/login";
+    }
+  }, []);
+
+  const renderDashboard = () => {
+    switch (role) {
+      case "Developer":
+        return <DashboardDev />;
+      default:
+        return <DashboardPM />;
+    }
+  };
+
+  if (!role) {
+    return <p style={{ textAlign: "center", marginTop: "50px" }}>Loading...</p>;
+  }
+
   return (
-<div style={styles.container}>
-<Sidebar />
-<div style={styles.main}>
-<Header role={role} />
-<DashboardContent />
-</div>
-</div>
+    <div style={styles.container}>
+      <Sidebar role={role} />
+      <div style={styles.main}>
+        <Header role={role} />
+        {renderDashboard()}
+      </div>
+    </div>
   );
 }
- 
+
 const styles = {
   container: {
     display: "flex",
@@ -28,5 +55,5 @@ const styles = {
     flexDirection: "column",
   },
 };
- 
+
 export default Dashboard;
